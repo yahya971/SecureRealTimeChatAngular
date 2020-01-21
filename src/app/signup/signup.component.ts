e8ccb4fef12f092665f668983e813e0e15f821ac
Yahya Derbeli <derbeliy97@gmail.com>
2020-01-21 11:23:19 +0100

    Some Changes

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  constructor(private fb: FormBuilder,private router:Router) { }
+  myForm: FormGroup;
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      cn: [''],
+      givenName: [''],
+      sn: [''],
+      telephoneNumber: [''],
+      userPassword:['']
+    });
 
-  ngOnInit() {}
+
+
+ 
+
+
+  }
+  submit() {
+    console.log(this.myForm.value);
+    this.router.navigate(['/login']);
+  }
+
 }
