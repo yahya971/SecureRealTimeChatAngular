b49084db0ba1a153bb0d827b2e140ae673fb36bc
karouiamine <aminekaroui7@gmail.com>
2020-01-23 15:11:25 +0100

    Added login in authService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../core/auth.service';
+import { AuthService } from '../_services/auth.service';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,12 +9,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  error: string;
 
-  constructor(readonly router: Router) {}
+  constructor(readonly router: Router, private authService: AuthService) {}
 
   login(formulaire: NgForm) {
-    console.log(formulaire.value);
+    this.authService.login(formulaire.value).subscribe(data => {
+      console.log(data);
+    })
     this.router.navigateByUrl('/chat');
   }
 }
